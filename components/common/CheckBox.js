9ef781bf31f65c6baf361bcba58aa2788a32fa88
hyundang <hyunjin5697@yonsei.ac.kr>
2021-02-26 17:47:02 +0900

    feat: 약관동의 구현

--- components/common/CheckBox.js
+++ components/common/CheckBox.js
@@ -1,22 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
-export default () => {
-    const [isClick, setIsClick] = useState(false);
-    
-    return(
-        <>
-            <Wrap/>
-        </>
-    )
-}
+export default ({className, checked, ...props}) => (
+    <CheckBoxContainer className={className}>
+        <HiddenCheckBox type={'checkbox'} checked={checked} {...props}/>
+        <StyledCheckBox checked={checked}>
+            <Icon viewBox="0 0 24 24">
+                <polyline points="19 7 10 17 5 12"/>
+            </Icon>
+        </StyledCheckBox>
+    </CheckBoxContainer>
+)
 
-const Wrap = styled.input.attrs({
-    type: 'checkbox',
-})`
-    width: 2rem;
-    height: 2rem;
-    border-radius: 0.4rem;
-    border: solid 0.1rem #dddddd;
-    color: red;
+const CheckBoxContainer = styled.div`
+display: inline-block;
+vertical-align: middle;
+`;
+
+const Icon = styled.svg`
+fill: none;
+stroke: white;
+stroke-width: 2px;
+`;
+
+const HiddenCheckBox = styled.input`
+border: 0;
+clip: rect(0 0 0 0);
+clippath: inset(50%);
+height: 1px;
+margin: -1px;
+overflow: hidden;
+padding: 0;
+position: absolute;
+white-space: nowrap;
+width: 1px;
+`;
+
+const StyledCheckBox = styled.div`
+display: inline-block;
+width: 2rem;
+height: 2rem;
+border: ${props => props.checked ? 'none' : 'solid 0.1rem #dddddd'};
+background: ${props => props.checked ? '#a99174' : 'white'};
+border-radius: 0.4rem;
+transition: all 150ms;
+
+${Icon} {
+    visibility: ${props=>props.checked? 'visible': 'hidden'};
+}
 `;
--- components/last/PageOne.js
+++ components/last/PageOne.js
@@ -1,23 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 // components
 import { Header } from "../../components";
-import { InputBox, Question } from "../../components/common";
+import { InputBox, OverlapBtnTwo, Question, CheckBox } from "../../components/common";
 // hooks
 import useInput from "../../hooks/useInput";
 // router
 import { useRouter } from "next/router";
+// assets
+import arrow from "../../assets/img/icons/brown_arrow_right.svg";
 
 
 export default () => {
     // 5개 만들기
     const input = useInput("");
+    // const checked = useInput(false);
 
     const router = useRouter();
     const handleClick = () => {
         // 스타일링 끝내기 눌렀을 때
         router.push('/website_dev/result');
     }
+
+    const [checked, setChecked] = useState(false);
+    const [checkedList, setCheckedList] = useState([]);
+
+    const handleCheckboxClick = (e) => {
+        if(e.target.checked){
+            setChecked(e.target.checked);
+            setCheckedList(['1', '2', '3']);
+            // console.log(1);
+        }
+        else{
+            setChecked(e.target.checked);
+            setCheckedList([]);
+        }
+    }
+
+    const handleCheckClick = (e) => {
+        if(checkedList.includes(e.target.id)){
+            setCheckedList(checkedList.filter((item, idx)=>{
+                return e.target.id != item;
+            }))
+        }
+        else{
+            setCheckedList(checkedList.concat(e.target.id))
+        }
+    }
+    
+    useEffect(()=>{
+        if(checkedList.length === 3){
+            setChecked(true);
+        }
+        else{
+            setChecked(false);
+        }
+    },[checkedList])
 
     return(
         <Wrap>
@@ -75,6 +113,37 @@ export default () => {
                 text={"예) instagram"}
                 input={input}
             />
+            {/* <OverlapBtnTwo/> 직업태그넣기 */}
+            <div style={{marginTop:'3.6rem'}}/>
+            <Question
+                quesNum={0}
+                quesText={"약관동의"}
+            />
+            <label style={{width:'32rem',marginTop:'2.1rem',marginBottom:'2.1rem'}}>
+                <CheckBox 
+                    checked={checked} 
+                    onChange={handleCheckboxClick}
+                />
+                <span style={{fontSize:'1.4rem',color:'#333333',textAlign:'left',marginLeft:'0.8rem'}}>전체동의</span>
+            </label>
+            <PolicyBox
+                id={1}
+                text={"리사이즈 이용약관"}
+                checkedList={checkedList} 
+                handleCheckClick={handleCheckClick}
+            />
+            <PolicyBox
+                id={2}
+                text={"리사이즈 개인 정보 처리방침"}
+                checkedList={checkedList} 
+                handleCheckClick={handleCheckClick}
+            />
+            <PolicyBox
+                id={3}
+                text={"[선택] 마케팅 활용동의"}
+                checkedList={checkedList} 
+                handleCheckClick={handleCheckClick}
+            />
             <div style={{marginTop:'3.6rem'}}/>
             <Bottom onClick={handleClick}>스타일링 끝내기</Bottom>
         </Wrap>
@@ -107,4 +176,45 @@ const Bottom = styled.div`
     color: black;
     letter-spacing: -0.4px;
 
+`;
+
+const PolicyBox = ({id, checkedList, handleCheckClick, text}) => {
+    return(
+        <BoxWrap>
+            <div style={{width:'2rem',height:'2rem',marginLeft:'1.3rem'}} >
+                <Icon viewBox="0 0 24 24" checked={checkedList.includes(`${id}`)?true:false} onClick={handleCheckClick} id={id}>
+                    <polyline points="19 7 10 17 5 12"/>
+                </Icon>
+            </div>
+            <Text>{text}</Text>
+            <img src={arrow} style={{width:'4rem',height:'4rem'}}/>
+        </BoxWrap>
+    )
+}
+
+const BoxWrap = styled.div`
+    width: 32.7rem;
+    height: 4.8rem;
+    margin-bottom: 0.8rem;
+    border-radius: 0.3rem;
+    border: solid 0.1rem #f5f3f0;
+    background-color: white;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const Icon = styled.svg`
+fill: none;
+stroke: ${props=>props.checked? '#a99174': 'black'};
+stroke-width: 2px;
+`;
+
+const Text = styled.span`
+    width: 26.4rem;
+    height: 1.8rem;
+    margin-left: 0.7rem;
+    font-size: 1.2rem;
+    text-align: left;
+    color: ${({theme})=>theme.colors.black};
 `;
