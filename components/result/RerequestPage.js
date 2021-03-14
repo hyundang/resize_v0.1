d705c38e36f973737dccf20f0e463721b237c9dd
hyundang <hyunjin5697@yonsei.ac.kr>
2021-03-14 14:50:35 +0900

    feat: 재요청 페이지 post 연결

--- components/result/RerequestPage.js
+++ components/result/RerequestPage.js
@@ -6,20 +6,23 @@ import { InputBoxBig } from "../common";
 // hooks
 import useInput from "../../hooks/useRecoilInput";
 // recoil
-import { useSetRecoilState, useRecoilState } from "recoil";
-import { RequestReasonState, RequestDetailState } from "../../states/result_atom"
+import { useRecoilState, useRecoilValue } from "recoil";
+import { RequestReasonState, RequestDetailState, CurationDataState } from "../../states/result_atom"
 // router
 import { useRouter } from "next/router";
-
+// api
+import { postApi } from "../../lib/api";
 
 
 const data=["색상이 마음에 안 들어서", '디자인이 마음에 안 들어서', '가격이 마음에 안 들어서', '품질이 마음에 안 들어서', '기타'];
 
-export default () => {
+export default ({id, sex}) => {
     const inputOne = useInput(RequestDetailState(0));
     const inputTwo = useInput(RequestDetailState(1))
     const [selectData, setSelectData] = useRecoilState(RequestReasonState);
     
+    const resData = useRecoilValue(CurationDataState);
+
     const router = useRouter();
     
     const [isActive, setIsActive] = useState(false);
@@ -38,6 +41,42 @@ export default () => {
         }
     }, [selectData, inputOne.value, inputTwo.value])
 
+
+    const PostData = async () => {
+        let data = resData.reservation;
+        const size_id =  data.size.id;
+
+        let post_data = {};
+        for( const [key, value] of Object.entries(data)){
+            if(key!=="create_time" & key!=="last_update_time"){
+                if(key==="size"){
+                    post_data[key] = size_id;
+                }
+                else if(key==="is_rereservation"){
+                    post_data[key] = "True";
+                }
+                else if(key==="reason"){
+                    post_data[key] = selectData;
+                }
+                else if(key==="reason_detail1"){
+                    post_data[key] = inputOne.value;
+                }
+                else if(key==="reason_detail2"){
+                    post_data[key] = inputTwo.value;
+                }
+                else{
+                    post_data[key] = value;
+                }
+            }
+        }
+
+        await postApi.PostData(post_data, sex, 'cody', 'Cody');
+    }
+
+    const handleClick = () => {
+        PostData();
+        router.push('/website_dev/result')
+    }
 
     return(
         <div style={{display:"flex", flexDirection:"column", alignItems:"center",overflow:'scroll'}}>
@@ -69,7 +108,7 @@ export default () => {
             </Wrap>
             <Bottom 
                 text={"코디 큐레이션 재요청하기"}
-                onClick={isActive?()=>router.push('/website_dev/result') : ()=>setIsModalShown(true)}
+                onClick={isActive? handleClick : ()=>setIsModalShown(true)}
                 isActive={isActive}
             />
             {isModalShown && <Modal
