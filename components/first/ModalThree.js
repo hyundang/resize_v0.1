2152620ed1a569b9fa15579b9f9fc7f8a0dbd759
hyundang <hyunjin5697@yonsei.ac.kr>
2021-03-21 16:52:15 +0900

    feat: 이용약관 모달 추가

---
+++ components/first/ModalThree.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+// components
+import { Modal } from "../../components/common";
+
+
+export default ({setModalVisible}) => {
+    return(
+        <Modal setModalVisible={setModalVisible}>
+            <div style={{fontSize:'1.8rem',fontWeight:'bold'}}>리사이즈 홍보 및 마케팅 이용 동의</div>
+            <div style={{height:'1.5rem'}}/>
+            <div>
+                귀하는 개인(신용)정보의 선택적인 수집∙이용, 
+                제공에 대한 동의를 거부할 수 있습니다. 
+                다만, 동의하지 않을 경우 관련 편의제공(이벤트 안내, 공지사항, 할인행사)안내 등 
+                이용 목적에 따른 혜택에 제한이 있을 수 있습니다.
+            </div>
+            <div style={{height:'1.5rem'}}/>
+            <div>
+                그 밖에 계약과 관련된 불이익은 없으며 동의한 경우에도 귀하는 동의를 철회하거나 
+                마케팅 목적으로 귀하에게 연락하는 것을 중지하도록 요청할 수 있습니다.
+            </div>
+            <div style={{height:'1.5rem'}}/>
+            <div>1. 수집 및 이용목적</div>
+            <div style={{height:'1.5rem'}}/>
+            <div>
+                고객에 대한 편의제공, 서비스 발전, 개인 맞춤형 서비스 제공, 
+                귀사 및 제휴업체의 상품·서비스 안내 및 이용권유, 사은·판촉행사 등의 마케팅 활동, 
+                시장조사 및 상품·서비스 개발연구 등을 목적으로 수집·이용됩니다.
+            </div>
+            <div style={{height:'1.5rem'}}/>
+            <div>2. 수집 및 이용항목</div>
+            <div style={{height:'1.5rem'}}/>
+            <div>
+                - 개인식별정보: 성명, 아이디, 비밀번호, 휴대폰번호, 생년월일, 성별, 인스타그램ID, 이메일, 직업 등
+            </div>
+            <div style={{height:'1.5rem'}}/>
+            <div>3. 보유기간</div>
+            <div style={{height:'1.5rem'}}/>
+            <div>
+                보유기간은 동의일로부터 회원 탈퇴 혹은 마케팅 동의 해제 시까지 보유·이용이며 
+                자세한 내용은 개인정보처리방침에서 확인 가능합니다.
+            </div>
+        </Modal>
+    )
+}
--- components/first/PolicyPage.js
+++ components/first/PolicyPage.js
@@ -6,7 +6,9 @@ import { PageNumState, CheckedListState } from "../../states/website_atom";
 // components
 import { Bottom } from "../../components";
 import { CheckBox, Question } from "../../components/common";
-import Modal from "../../components/common/modal";
+import ModalOne from './ModalOne';
+import ModalTwo from './ModalTwo';
+import ModalThree from './ModalThree';
 // assets
 import arrow from "../../public/images/website/icon/brown_arrow_right.svg";
 
@@ -21,7 +23,9 @@ export default ( ) => {
     // 다음 단계 진행 가능 여부
     const [isNextOkay, setIsNextOkay] = useState(false);
     // 모달
-    const [modalVisible, setModalVisible] = useState(false);
+    const [modalOneVisible, setModalOneVisible] = useState(false);
+    const [modalTwoVisible, setModalTwoVisible] = useState(false);
+    const [modalThreeVisible, setModalThreeVisible] = useState(false);
 
     const handleCheckboxClick = (e) => {
         if(e.target.checked){
@@ -76,24 +80,24 @@ export default ( ) => {
             </label>
             <PolicyBox
                 id={1}
-                text={"리사이즈 이용약관"}
+                text={"이용약관 동의"}
                 checkedList={checkedList} 
                 handleCheckClick={handleCheckClick}
-                handleBtnClick={()=>setModalVisible(true)}
+                handleBtnClick={()=>setModalOneVisible(true)}
             />
             <PolicyBox
                 id={2}
-                text={"리사이즈 개인 정보 처리방침"}
+                text={"개인정보 수집 및 이용 동의"}
                 checkedList={checkedList} 
                 handleCheckClick={handleCheckClick}
-                handleBtnClick={()=>setModalVisible(true)}
+                handleBtnClick={()=>setModalTwoVisible(true)}
             />
             <PolicyBox
                 id={3}
                 text={"[선택] 마케팅 활용동의"}
                 checkedList={checkedList} 
                 handleCheckClick={handleCheckClick}
-                handleBtnClick={()=>setModalVisible(true)}
+                handleBtnClick={()=>setModalThreeVisible(true)}
             />
             <div style={{height:'4.5rem'}}/>
             <Bottom
@@ -103,56 +107,9 @@ export default ( ) => {
                 isRightOkay={isNextOkay}
             />
         </Wrap>
-        {modalVisible&&
-        <Modal
-            visible={modalVisible}
-            closable={true}
-            maskClosable={true}
-            onClose={()=>setModalVisible(false)}
-            title= {'개인정보 처리방침'}
-        >
-            <div style={{height:'1.5rem'}}/>
-            <div style={{width:'26rem'}}>제 1조 [총칙]</div>
-            <div style={{height:'1.5rem'}}/>
-            <div>“리사이즈”는 이용자들의 개인정보를 중요시하며 고객들의 개인정보를 보호하기 
-                위해 최선을 다하고 있습니다. 따라서 「통신비밀보호법」,
-                「정보통신망 이용촉진 및 정보보호 등에 관한 법률」 등과 관련된 법규를 
-                준수하기 위해 [개인정보 처리방침]을 제정하고 이를 준수하고 있습니다.
-            </div>
-            <div style={{height:'1.5rem'}}/>
-            <div>리사이즈는 다음 개인정보 처리방침을 통하여 이용자들의 개인정보가 
-                어떠한 용도로 이용되고 있으며 어떠한 방식으로 관리되고 있는지 또한 
-                개인정보보호를 위해 어떠한 조치가 취해지고 있는지 알려 드립니다.
-            </div>
-            <div style={{height:'1.5rem'}}/>
-            <div>
-                리사이즈의 개인정보 처리방침은 관련 법률 및 지침의 변경이나 회사 내부 운영방침의 
-                변경 등으로 인하여 개정될 수 있습니다. 개인정보 처리방침 이 변경될 경우 변경사항은 
-                홈페이지에 게시됩니다. 
-            </div>
-            <div style={{height:'1.5rem'}}/>
-            <div style={{width:'26rem'}}>제 2 조 [개인정보 수집항목 및 이용목적]</div>
-            <div style={{height:'1.5rem'}}/>
-            <div>
-                리사이즈가 개인정보 보호법 제32조에 따라 등록․공개하는 개인정보파일의 처리목적은 
-                다음과 같습니다.
-            </div>
-            <div style={{height:'1.5rem'}}/>
-            <div>
-                - (필수) 성명, 아이디, 비밀번호, 휴대폰번호, 생년월일 : 회원제 서비스 이용에 따른 본인 확인 절차에 이용
-            </div>
-            <div>
-            - (필수) 기타 선택항목(신체사이즈, 선호 스타일 등) : 개인맞춤 서비스를 제공하기 위한 자료
-            </div>
-            <div>
-                - (필수) IP 주소, 방문일시, 쿠키, 기기정보 등 서비스 이용기록 : 서비스 개선, 불량회원 관리
-            </div>
-            <div style={{height:'1.5rem'}}/>
-            <div>
-                수집된 정보는 서비스의 발전, 알고리즘 개발, 회원관리, 마케팅 및 광고 등에 활용됩니다.
-            </div>
-        </Modal>
-        }
+        {modalOneVisible&&<ModalOne setModalVisible={setModalOneVisible}/>}
+        {modalTwoVisible&&<ModalTwo setModalVisible={setModalTwoVisible}/>}
+        {modalThreeVisible&&<ModalThree setModalVisible={setModalThreeVisible}/>}
         </>
     )
 }
