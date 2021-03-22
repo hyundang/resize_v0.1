cae936d39fecff18a0a8bb42ecf269b605ab1145
hyundang <hyunjin5697@yonsei.ac.kr>
2021-03-23 01:05:43 +0900

    fix: img 변경

--- pages/_app.js
+++ pages/_app.js
@@ -61,7 +61,7 @@ const Resize = ({ Component, pageProps }) => {
           <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&family=Noto+Serif+KR:wght@200;300;400;500&display=swap&family=Nanum+Myeongjo:wght@400;700&display=swap" rel="stylesheet"/> 
           <meta name="title" property="og:title" content="RESIZE" />
           <meta name="description" property="og:description" content="오직 나만을 위한 모바일 퍼스널 쇼퍼"/>
-          <meta name="image" property="og:image" content="/images/website/kakao-ogtag2.jpg" />
+          <meta name="image" property="og:image" content="/images/website/ogtag_img.jpg" />
           <meta property="og:url" content="https://resize.co.kr" />
           <meta property="og:site_name" content="RESIZE" />
         </Head>
