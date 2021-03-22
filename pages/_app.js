06883ecdcb062fb1cd2fcd251b9c6966b772e23a
hyundang <hyunjin5697@yonsei.ac.kr>
2021-03-23 00:51:49 +0900

    fix: 수정사항 반영

--- pages/_app.js
+++ pages/_app.js
@@ -59,9 +59,11 @@ const Resize = ({ Component, pageProps }) => {
           <link rel="shortcut icon" href="/images/resizeLogo.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&family=Noto+Serif+KR:wght@200;300;400;500&display=swap&family=Nanum+Myeongjo:wght@400;700&display=swap" rel="stylesheet"/> 
-          <meta name="title" property="og:title" content="체형 유형 테스트" />
-          <meta name="description" property="og:description" content="나와 비슷한 체형의 동물이 존재한다구? 나도 몰랐던 체형유형과 이에 맞는 찰떡 코디 추천까지!"/>
-          <meta name="image" property="og:image" content="/images/sizetest/preview.png" />
+          <meta name="title" property="og:title" content="RESIZE" />
+          <meta name="description" property="og:description" content="오직 나만을 위한 모바일 퍼스널 쇼퍼"/>
+          <meta name="image" property="og:image" content="/images/website/background.png" />
+          <meta property="og:url" content="https://resize.co.kr" />
+          <meta property="og:site_name" content="RESIZE" />
         </Head>
         {isPC?
         <>
