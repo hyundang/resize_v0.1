8d816d6b34b3497d1a4c92884ee149591ad1862b
hyundang <hyunjin5697@yonsei.ac.kr>
2021-04-08 00:36:42 +0900

    fix: pc 방지

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 // components
 import { 
@@ -6,6 +6,8 @@ import {
     GuidePage, LoginPage,
     PolicyPage, InformPage, 
 } from "../components/first";
+import { PC, Mobile } from "../lib/mediaQuery";
+import { PreparePage, PreparePagePC } from "../components";
 // recoil
 import { useRecoilState, useRecoilValue } from "recoil";
 import { PageNumState, VisitState } from "../states/website_atom";
@@ -15,6 +17,19 @@ const Start  = () => {
     const [pageNum, setPageNum] = useRecoilState(PageNumState);
     const isVisited = useRecoilValue(VisitState);
 
+
+    let filter = "win16|win32|win64|mac|macintel";
+    const [isPC, setIsPC] = useState(true);
+
+    // 접속 기기가 pc인지 확인
+    useEffect(()=>{
+      if(navigator.platform) {
+        if(filter.indexOf(navigator.platform.toLowerCase())<0){
+          setIsPC(false);
+          console.log(isPC);
+        }
+      }
+    }, [])
     // switch (pageNum) {
     //     case 1:
     //         return <PageTwo
@@ -45,10 +60,21 @@ const Start  = () => {
     //                     setPageNum={setPageNum}
     //                 />
     // }
-    return(
+    if(isPC){
+        return (
         <>
+            <PC>
+                <PreparePagePC/>
+            </PC>
+            <Mobile>
+                <PreparePage/>
+            </Mobile>
         </>
-    )
+        )
+    }
+    else{
+        <></>
+    }
 }
 
 export default Start;
